b811203edf5c5a4292c0f33ce3003712671cdb60
Gregor Gilchrist <gregorgilchrist@gmail.com>
2023-10-26 22:32:15 +0200

    crystal balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,23 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    // [ false, false, false, false, false, true, true, true, true, true, true]
+    const jumpAmount = Math.floor(Math.sqrt(breaks.length));
 
+    let i = jumpAmount;
+
+    for (; i < breaks.length; i += jumpAmount) {
+        if (breaks[i]) {
+            // === true
+            break;
+        }
+    }
+
+    i -= jumpAmount;
+
+    for (let j = 0; j < jumpAmount && i < breaks.length; j++, i++) {
+        if (breaks[i] === true) {
+            return i;
+        }
+    }
+
+    return -1;
 }
